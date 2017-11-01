a89718cad6470365ef1c6f6aa6420254bff4b17c
doaa-altarawy <doaa.altarawy@gmail.com>
2017-11-01 16:59:15 -0400

    PEP8 fixes

--- molssi_api_flask/__init__.py
+++ molssi_api_flask/__init__.py
@@ -14,18 +14,22 @@ print(app.config['APPLICATION_ROOT'])
 #  MongoDB connection
 db.get_connection(host=app.config['DB_URI'])
 
+
 @app.route('/static/<path:path>')
 def send_js(path):
     return send_from_directory('static', path)
 
+
 @app.errorhandler(404)
 def not_found(error):
-  return render_template('404.html'), 404
+    return render_template('404.html'), 404
+
 
 from molssi_api_flask.core.views import mod as core
 app.register_blueprint(core)
 
-#app.register_blueprint(core, url_prefix='/api')
+
+# app.register_blueprint(core, url_prefix='/api')
 
 # Later on you'll import the other blueprints the same way:
 #from app.comments.views import mod as commentsModule
