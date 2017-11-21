2231ec1db3093528572d1e5c5c0aebba8bcbe2f0
doaa-altarawy <doaa.altarawy@gmail.com>
2017-11-21 16:44:31 -0500

    Load data using new config object

--- load_DB.py
+++ load_DB.py
@@ -1,12 +1,14 @@
 #!venv/bin/python
 
 from __future__ import print_function
-from molssi_api_flask.core import mongo_database
+from molssi_api_flask.models import mongo_database
 import os
 import config as config
 
+config = config.DevelopmentConfig()
+
 #  MongoDB connection
-db = mongo_database.get_connection(config.MONGODB_SETTINGS['host'])
+db = mongo_database.get_connection(host=config.MONGODB_SETTINGS['host'])
 print('Connecting to DB: ', config.MONGODB_SETTINGS['host'])
 
 #  Clear the DB
@@ -18,17 +20,17 @@ MM_files = ['MM_curated_data.json']
 QM_files = ['QM_curated_data.json']
 
 
-print('Initial DB size: {}'.format(mongo_database.full_search().count()))
+print('Initial DB size: {}'.format(mongo_database.get_DB_size()))
 for filename in MM_files:
     full_path = os.path.join(config.APPLICATION_ROOT, 'data', filename)
     print('Loading file {} in DB....'.format(full_path))
     mongo_database.load_collection_from_json(full_path)
 
-    print('After insertion: {}'.format(mongo_database.full_search().count()))
+    print('After insertion: {}'.format(mongo_database.get_DB_size()))
 
 for filename in QM_files:
     full_path = os.path.join(config.APPLICATION_ROOT, 'data', filename)
     print('Loading file {} in DB....'.format(full_path))
     mongo_database.load_collection_from_json(full_path)
 
-    print('After insertion: {}'.format(mongo_database.full_search().count()))
+    print('After insertion: {}'.format(mongo_database.get_DB_size()))
