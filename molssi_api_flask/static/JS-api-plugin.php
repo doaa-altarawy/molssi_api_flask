94587443e9d575a623ff791347aeca40a80354b3
doaa-altarawy <doaa.altarawy@gmail.com>
2017-12-19 15:24:05 -0500

    Worpress plugin for resize and scrol top

---
+++ molssi_api_flask/static/JS-api-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+Plugin Name: iFrame scrol and resize
+*/
+add_action( 'wp_head', 'add_js_scripts' );
+function add_js_scripts(){
+  ?>
+  <script>console.log('iFrame scrol and resize added'); </script>
+  <script src="http://api.molssi.org/static/js/wordpress_parent.js" type="text/javascript"></script>
+  <?php
+  // <script src="http://localhost:5000/static/js/wordpress_parent.js" type="text/javascript"></script>
+}
