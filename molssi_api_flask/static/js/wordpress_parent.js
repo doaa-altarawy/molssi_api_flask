57aabf6880acafffd16760b8457a6ce6b31bfb30
Doaa Altarawy <doaa@byrch.dreamhost.com>
2017-12-19 12:43:01 -0800

    Fix inArray comparision

--- molssi_api_flask/static/js/wordpress_parent.js
+++ molssi_api_flask/static/js/wordpress_parent.js
@@ -1,7 +1,7 @@
 // <iframe id="pageContent" width="100%" height="4000px" src="http://api.molssi.org/resources_website" frameborder="0"></iframe>
 function handleMessage(event) {
     var apiOrigins = ['http://api.molssi.org', 'http://localhost:5000'];
-    if (jQuery.inArray(event.origin, apiOrigins)) {
+    if (jQuery.inArray(event.origin, apiOrigins) != -1) {
         console.log("Event from iframe:", event.data['task']);
         if (event.data['task'] == 'resize') {
             jQuery('#pageContent').get(0).height = event.data['height'] + 'px';   // adjust height
