5892db6578c2261618a222f977204f5b940f8aa3
doaa-altarawy <doaa.altarawy@gmail.com>
2018-08-09 16:15:03 -0400

    Return 404 error as json on request

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,7 +1,18 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from . import main
 
 
-@main.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'), 404
+# @main.errorhandler(404)
+# def not_found(error):
+#     return render_template('404.html'), 404
+
+
+@main.app_errorhandler(404)
+def page_not_found(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'not found'})
+        response.status_code = 404
+        return response
+    else:
+        return render_template('404.html'), 404
