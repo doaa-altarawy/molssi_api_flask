30e4470356b81e669b77a7b15177c4a541644d95
doaa-altarawy <doaa.altarawy@gmail.com>
2018-08-09 16:10:10 -0400

    Update setup with test requirements

--- setup.py
+++ setup.py
@@ -1,23 +1,48 @@
-#! /usr/bin/env python
+import setuptools
 
-from setuptools import setup, find_packages
+if __name__ == "__main__":
+    setuptools.setup(
+        name='molssi_api_flask',
+        version="0.3.0",
+        description='MolSSI code database',
+        author='Doaa Altarawy',
+        author_email='daltarawy@vt.edu',
+        url="https://github.com/doaa-altarawy/molssi_api_flask",
+        license='BSD-3C',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'Flask==0.12.2',
+            'Flask_Cors==3.0.3',
+            'Flask-Jsonpify==1.5.0',
+            'pymongo',
+            'mongoengine',
+        ],
+        extras_require={
+            'docs': [
+                'sphinx', 
+                'sphinxcontrib-napoleon',
+                'sphinx_rtd_theme',
+                'numpydoc',
+            ],
+            'tests': [
+                'pytest>=3.0',
+                'codecov',
+                'pytest-cov',
+                'pytest-pep8',
+            ],
+        },
 
-setup(
-    name="molssi_api_flask",
-    version="1.0.0",
-    author="Doaa Altarawy",
-    description="MolSSI API interface",
-    long_description=__doc__,
-    license="MIT",
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'Flask==0.12.2',
-        'Flask_Cors==3.0.3',
-        'Flask-Jsonpify==1.5.0',
-        'pymongo',
-        'mongoengine',
-    ],
-    tests_require=['pytest', 'mongomock'],
-)
+        # tests_require=[
+        #     'pytest',
+        #     'pytest-cov',
+        #     'pytest-pep8',
+        # ],
+
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.5',
+        ],
+        zip_safe=True,
+    )
