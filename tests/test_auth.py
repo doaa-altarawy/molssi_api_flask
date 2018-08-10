3defa0cabbed1c2bc5d986e2c6c6b0c8149cab4e
doaa-altarawy <doaa.altarawy@gmail.com>
2018-08-10 11:48:24 -0400

    Tests change email and password

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -53,6 +53,14 @@ class TestAuth(object):
             'Content-Type': 'application/json'
         }
 
+    def login_admin(self):
+        self.client.get(self.auth_url+'/logout', follow_redirects=True)
+        data = dict(email='daltarawy@vt.edu', password='fakePass')
+        response = self.client.post(self.auth_url+'/login', data=data,
+                                    follow_redirects=True)
+        success = 'Change Email' in response.get_data(as_text=True)
+        return success
+
     def test_register(self):
         # get registration form
         response = self.client.get((self.auth_url+'/register'))
@@ -101,3 +109,61 @@ class TestAuth(object):
         response = self.client.get(self.auth_url+'/logout',
                                    follow_redirects=True)
         assert 'You have been logged out' in response.get_data(as_text=True)
+
+    def test_change_password(self):
+        # log in as admin
+        assert self.login_admin()
+        response = self.client.get(self.auth_url+'/change-password')
+        assert response.status_code == 200
+        assert 'Old password' in response.get_data(as_text=True)
+
+        # new password does not match
+        data = dict(old_password='wrong', password='123', password2='456')
+        response = self.client.post(self.auth_url+'/change-password', data=data)
+        assert response.status_code == 200
+        assert 'Passwords must match' in response.get_data(as_text=True)
+
+        # wrong old password
+        data = dict(old_password='wrong', password='1234567', password2='1234567')
+        response = self.client.post(self.auth_url+'/change-password', data=data)
+        assert response.status_code == 200
+        assert 'Invalid password' in response.get_data(as_text=True)
+
+        # correct entry
+        data = dict(old_password='fakePass', password='1234567', password2='1234567')
+        response = self.client.post(self.auth_url+'/change-password', data=data,
+                                    follow_redirects=True)
+        assert response.status_code == 200
+        assert 'Your password has been updated' in response.get_data(as_text=True)
+
+        # change password back to original, redirect of success
+        data = dict(old_password='1234567', password='fakePass', password2='fakePass')
+        response = self.client.post(self.auth_url+'/change-password', data=data)
+        assert response.status_code == 302
+
+    def test_change_email(self):
+        # log in as admin
+        assert self.login_admin()
+        response = self.client.get(self.auth_url+'/change_email')
+        assert response.status_code == 200
+        assert 'Change Your Email Address' in response.get_data(as_text=True)
+
+        # Email already exist
+        data = dict(email='daltarawy@vt.edu', password='fakePass')
+        response = self.client.post(self.auth_url+'/change_email', data=data)
+        assert response.status_code == 200
+        assert 'Email already registered' in response.get_data(as_text=True)
+
+        # Wrong password
+        data = dict(email='daltarawy2@vt.edu', password='wrong')
+        response = self.client.post(self.auth_url+'/change_email', data=data)
+        assert response.status_code == 200
+        assert 'Invalid password' in response.get_data(as_text=True)
+
+        # correct entry
+        data = dict(email='daltarawy2@vt.edu', password='fakePass')
+        response = self.client.post(self.auth_url+'/change_email', data=data,
+                                    follow_redirects=True)
+        assert response.status_code == 200
+        assert 'confirm your new email address' in response.get_data(as_text=True)
+ 
